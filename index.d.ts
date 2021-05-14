c5e23344d2d6daaf97f19665880e03d6b071c4d2
mengdaoshizhongxinyang <729403918@qq.com>
2021-05-14 14:06:57 +0800

    chore:update npmignore

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { DefineComponent } from 'vue'
-import { VueTypeValidableDef } from "vue-types";
-declare const _default: DefineComponent<{
-    source:VueTypeValidableDef<string> & {
-        default:""
-    }
-}, {}, any>
-// export default component
